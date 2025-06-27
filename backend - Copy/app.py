686dde057cac36020df61f596093426c59578bd7
rt23-dev <rohantaneja.23.359@gmail.com>
2025-06-27 16:32:25 +0530

    Update app.py

--- backend - Copy/app.py
+++ backend - Copy/app.py
@@ -42,8 +42,7 @@ def analyze():
 
 print("✅ Flask app starting...")
 
-import os
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
